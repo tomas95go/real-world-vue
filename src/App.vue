9b0748d6fde19fd5341448289755e5f068d9c8f9
Tomas Gonzalez Oviedo <tomas95go@gmail.com>
2021-04-29 23:49:46 -0300

    L4-end with Bulma

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <section class="section">
     <div class="container">
       <div class="columns is-centered">
-        <router-link class="button is-outlined is-link mr-1" to="/"> Home </router-link>
+        <router-link class="button is-outlined is-link mr-1" to="/"> Event List </router-link>
         <router-link class="button is-outlined is-link ml-1" to="/about"> About </router-link>
       </div>
       <div class="columns is-centered">
